77012a5841ee5ed6f613610a8abe681b4f2f7094
Mansi Jain <supermansi@users.noreply.github.com>
2018-08-13 20:25:48 -0400

    modified questions schema for multiple choice

--- models/questions/questions.schema.server.js
+++ models/questions/questions.schema.server.js
@@ -3,6 +3,11 @@ module.exports = mongoose.Schema({
     title: String,
     points: Number,
     description: String,
+    choices: [{
+        text: String,
+        value: String,
+        correct: Boolean
+    }],
     questionType: {
         type: String,
         enum: [ 'ESSAY',
